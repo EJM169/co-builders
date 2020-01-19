5818b29f4ae406b2b13330aad7c73aa133d7f5b6
Evin J Manuel <evinjmanuel15@gmail.com>
2020-01-20 02:31:34 +0530

    improved signup form

--- public/js/mail-script.js
+++ public/js/mail-script.js
@@ -4,6 +4,17 @@
         var form = $('#myForm'); // contact form
         var submit = $('.submit-btn'); // submit button
         var alert = $('.alert-msg'); // alert div for show alert message
+        var for_pass=$('#for_pass'); //Forgot password button
+        var for_pop=$('#for_pop'); //Forgot password body
+        var close=$('#close');
+        var custpass1=$('#cust_pass1');
+        var custpass2=$('#cust_pass2');
+        var pass_match1=$('#pass_match1');
+        var contpass1=$('#cont_pass1');
+        var contpass2=$('#cont_pass2');
+        var pass_match2=$('#pass_match2');
+
+
 
         // form submit event
         form.on('submit', function(e) {
@@ -28,4 +39,31 @@
                 }
             });
         });
+
+    for_pass.click(function(){
+        for_pop.fadeIn(500);
+    })
+    close.click(function(){
+        for_pop.fadeOut(500);
+    })
+    custpass2.keyup(function(){
+        console.log(custpass1.val());
+        if(custpass1.val() != custpass2.val()){
+            pass_match1.fadeIn(500);
+        }
+        else{
+            pass_match1.fadeOut(500);
+        }
+        return true;
+    })
+    contpass2.keyup(function(){
+        console.log(custpass1.val());
+        if(contpass1.val() != contpass2.val()){
+            pass_match2.fadeIn(500);
+        }
+        else{
+            pass_match2.fadeOut(500);
+        }
+        return true;
+    })
     });
