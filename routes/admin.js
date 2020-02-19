aab0001c9f083e66fcc3e7f4ad073a54c2cde288
Evin J Manuel <evinjmanuel15@gmail.com>
2020-02-19 14:42:57 +0530

    made some more modification

--- routes/admin.js
+++ routes/admin.js
@@ -4,12 +4,48 @@ var express             = require("express"),
     contractorUser      = require("../models/contractor"),
     customerUser        = require("../models/customer"),
     projectC            = require("../models/project"),
+    admin               = require("../models/admin"),
     flash               = require("connect-flash"),
     passport            = require("passport"),
     LocalStrategy       = require("passport-local"),
     bCrypt              = require('bcrypt'),
     middleware          = require("../middleware");
 
+    var isValidPassword = function(user, password){
+        return bCrypt.compareSync(password, user.password);
+      }
+      passport.use("admin-login",new LocalStrategy(
+        {    
+            usernameField:'username',
+            passReqToCallback : true
+        }, function(req, username, password, done) { 
+        // check in mongo if a user with username exists or not
+        admin.findOne({'username' : username}, 
+          function(err, user) {
+            // In case of any error, return using the done method
+            if (err)
+              return done(err);
+            // Username does not exist, log error & redirect back
+            if (!user){
+              console.log('User Not Found with username '+username);
+              return done(null, false, 
+                    req.flash('error', 'User Not found.')
+                    );                 
+            }
+            // User exists but wrong password, log the error 
+            if (!isValidPassword(user, password)){
+              console.log('Invalid Password');
+              return done(null, false, 
+                  req.flash('error', 'Invalid Password')
+                  );
+            }
+            // User and password both match, return user from 
+            // done method which will be treated like success
+            req.flash('success','Welcome '+user.username);
+            return done(null, user);
+          }
+        );}));
+        
 
     router.get("/panel/169/admin",function(req,res){
         res.render("admin");
@@ -20,4 +56,8 @@ var express             = require("express"),
         failureRedirect:"/panel/169/admin",
         failureFlash : true 
       }));
+    router.get("/panel/169/admin/dashboard",function(req,res){
+        
+
+    })  
 module.exports = router;
