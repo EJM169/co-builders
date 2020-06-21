5cc62c94ec74c428d341ecdd531418beb68cbc19
Evin J Manuel <evinjmanuel15@gmail.com>
2020-06-22 00:15:30 +0530

    made improvement to project complete contractor side

--- routes/contractor.js
+++ routes/contractor.js
@@ -601,9 +601,6 @@ router.post("/contractor/:id/schedule/:id1/delete",middleware.isContractorLogged
      res.redirect("/contractor/dashboard");  
    }
    else{
-        
-        
-         
             project.planDate.pull({_id:req.params.id1});
                   project.save(function(err,savedata){
                     if(err){
@@ -699,34 +696,78 @@ router.post("/contractor/:id/complete",middleware.isContractorLoggedIn,function(
           res.redirect("/contractor/dashboard");
         }   
         else{
-          if(project.flags.customerComplete){
-              project.flags.complete=!project.flags.complete;
-              project.flags.contractorComplete=!project.flags.contractorComplete;
-              project.save(function(err,savedata){
-                if(err){
-                  console.log(err);
-                  req.flash('error','Error while saving');
-                  res.redirect("/contractor/dashboard");
-                }
-                else{
-                  req.flash('success','Successfully saved the data');
-                  res.redirect("/contractor/"+project.contractor+"/customer")
-                }
-              })
-          }else{
-            project.flags.contractorComplete=!project.flags.contractorComplete;
-            project.save(function(err,savedata){
-              if(err){
-                console.log(err);
-                req.flash('error','Error while saving');
-                res.redirect("/contractor/dashboard");
+          customerUser.findById(project.customer,function(err,customer){
+            if(err){
+              console.log(err);
+              req.flash('error','Error whil loading details');
+              res.redirect("/contractor/dashboard");
+            }else{
+              if(project.flags.customerComplete){
+                async.series([
+                  function(callback){
+                    customer.active_proj_cont.pop(contractor);
+                    customer.past_proj_cont.push(contractor);
+                    customer.projectStatus=!customer.projectStatus;
+                    customer.save(function(err,data){
+                      if(err){
+                        console.log(err);
+                        callback(err);
+                      }
+                      callback();
+                    });
+                  },
+                  function(callback){
+                    contractor.active_proj_cust.pop(customer);
+                    contractor.past_proj_cust.push(customer);
+                    contractor.projectStatus=!contractor.projectStatus;
+                    contractor.no_project +=1;
+                    contractor.save(function(err,data){
+                      if(err){
+                        console.log(err)
+                        callback(err);
+                      }
+                      callback();
+                    });
+                  },
+                  function(callback){
+                    project.customerStatus=!project.customerStatus;
+                    project.projectStart=! project.projectStart;
+                    project.contractorStatus=!project.contractorStatus;
+                    project.flags.complete=!project.flags.complete;
+                    project.flags.contractorComplete=!project.flags.contractorComplete;
+                    project.save(function(err,savedata){
+                      if(err){
+                        console.log(err);
+                        callback(err);
+                      }
+                      callback()
+                    })
+                  }
+                ],function(err){
+                  if(err){
+                    req.flash('error','Error while saving all the data try again');
+                    res.redirect("/customer/"+customer._id+"/contractor");
+                  }
+                  req.flash('success','successfully saved and project is complete');
+                  res.redirect("/contractor/"+contractor._id+"/customer");
+                });
+                 
+              }else{
+                project.flags.contractorComplete=!project.flags.contractorComplete;
+                project.save(function(err,savedata){
+                  if(err){
+                    console.log(err);
+                    req.flash('error','Error while saving');
+                    res.redirect("/contractor/dashboard");
+                  }
+                  else{
+                    req.flash('success','Successfully saved the data');
+                    res.redirect("/contractor/"+project.contractor+"/customer")
+                  }
+                })
               }
-              else{
-                req.flash('success','Successfully saved the data');
-                res.redirect("/contractor/"+project.contractor+"/customer")
-              }
-            })
-          }
+            }
+          });
         }
       }); 
     }
